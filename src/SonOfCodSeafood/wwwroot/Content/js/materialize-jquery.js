efbaf2eddde7e2d86b8a51f6dd8db6d39264c84d
Steve Burton <stevevburton@gmail.comgit config --global user.email stevevburton@gmail.comgit config --global user.email stevevburton@gmail.com>
2017-02-17 09:54:16 -0800

    add Materialize CSS

---
+++ src/SonOfCodSeafood/wwwroot/Content/js/materialize-jquery.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function() {
+
+    $('.button-collapse').sideNav({
+        menuWidth: 200, // Default is 300
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    });
+
+    $('select').material_select();
+    $(".dropdown-button").dropdown();
+})
